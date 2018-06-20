216f1603573c1186785a5b5514206cdaaef25c04
Vivianne Cristina <vivianne.crisns@gmail.com>
2018-06-20 17:09:48 -0300

    exercicio in progress

--- app/src/main/java/com/challenges/login/LoginActivity.java
+++ app/src/main/java/com/challenges/login/LoginActivity.java
@@ -25,29 +25,33 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        user = findViewById(R.id.editUser);
-        senha = findViewById(R.id.editSenha);
-        salvar = findViewById(R.id.saveBtn);
-
         init();
 
-        salvar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                editor.putString("", user.getText().toString()).commit();
-            }
-        });
+        if (preferences.getBoolean("logado", false)) {
+            initActivity2();
 
-        salvar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                editor.putString("", senha.getText().toString()).commit();
-            }
-        });
+        }else{
+            user = findViewById(R.id.editUser);
+            senha = findViewById(R.id.editSenha);
+            salvar = findViewById(R.id.saveBtn);
 
+            salvar.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    editor.putString("User", user.getText().toString()).commit();
+                    editor.putString("Senha", senha.getText().toString()).commit();
+                    editor.putBoolean("logado", true).commit();
+                    initActivity2();
+                }
+            });
+        }
     }
     private void init () {
        preferences = getSharedPreferences("Login", Context.MODE_PRIVATE);
        editor = preferences.edit();
+    }
+
+    private void initActivity2 () {
+
     }
 }
